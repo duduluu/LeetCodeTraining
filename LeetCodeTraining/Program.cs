8eb8d13dcbe86c83ddd02256453532fc50b62c70
duduluu <duduluu@outlook.com>
2017-12-07 21:49:19 +0800

    添加项目文件。

---
+++ LeetCodeTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
